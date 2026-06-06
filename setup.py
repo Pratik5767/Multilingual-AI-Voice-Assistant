aae993de4b59bdd33e04ccbe833b6330dccdffbc
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-06-06 23:56:03 +0530

    added the setup file for loading modules locally

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="genai voice assistant project",
+    version="0.0.1",
+    author="Pratik Salunkhe",
+    author_email="pratikvsalunkhe924@gmail.com",
+    packages=find_packages()
+)
